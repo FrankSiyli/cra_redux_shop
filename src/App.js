1753743e98d3cde55b2c76d4af07456e464d1fd6
imac <frank.siyli@gmail.com>
2023-05-18 12:46:04 +0200

    static category buttons

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import { useGetAllProductsQuery } from "./redux/apis/productsApi";
 
 function App() {
   const { data: products, error, isLoading } = useGetAllProductsQuery();
-  console.log(products);
+const categoryNames = []
+
+products?.forEach((product)=> {
+if(!categoryNames.includes(product.category.name)){
+categoryNames.push(product.category.name)
+}})
+
 
   return (
     <div className="App">
-      {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : products ? (
+      {error && <h3>Oh no, there was an error</h3>}
+      {isLoading && <h3>Loading...</h3>}
+      <button>all</button>
+      <button>Furniture</button>
+      <button>Electronic</button>
+      <button>nuevo</button>
+      <button>Cosmatic</button>
+      <button>Shoes</button>
+      {products && (
         <>
           {products?.map((product) => (
             <p key={product.id}>{product.title}</p>
           ))}
         </>
-      ) : null}
+      )}
     </div>
   );
 }

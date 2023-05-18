273d3b8900c69c3542c3febe72985c036bd11424
imac <frank.siyli@gmail.com>
2023-05-18 16:50:30 +0200

    better login

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { login, logout } from "../../redux/slices/usersSlice";
-import { useState } from "react";
 
 const Users = () => {
   const dispatch = useDispatch();
   const username = useSelector((state) => state.users.value.username);
   const [newUsername, setNewUsername] = useState("");
+  const [isLoggedOut, setIsLoggedOut] = useState(true);
+
   const handleLogin = () => {
-    dispatch(login({ username: newUsername }));
-    setNewUsername("");
+    if (newUsername === "") {
+      alert("Please enter a username");
+    } else {
+      dispatch(login({ username: newUsername }));
+      setIsLoggedOut(false);
+      setNewUsername("");
+    }
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    setIsLoggedOut(true);
   };
 
   return (
@@ -19,9 +30,12 @@ const Users = () => {
         type="text"
         value={newUsername}
         onChange={(e) => setNewUsername(e.target.value)}
+        required
       />
-      <button onClick={handleLogin}>login</button>
-      <button onClick={() => dispatch(logout())}>logout</button>
+      <button onClick={handleLogin} disabled={!isLoggedOut}>
+        Login
+      </button>
+      <button onClick={handleLogout}>Logout</button>
       <hr />
     </>
   );
